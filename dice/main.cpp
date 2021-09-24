09d3efedd3fdf4c7ba4dc8229ccb284ef10ce8b9
nathdilla <nathcdilla@protonmail.com>
2021-09-24 12:25:20 -0700

    Initial Commit

---
+++ dice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dice
+//
+//  Created by Nathan Dilla on 9/24/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
